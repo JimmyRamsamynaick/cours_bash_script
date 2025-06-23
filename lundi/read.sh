70c7a1c872014a6db37e7bd92f023aa1ecd681d5
jimmy <jimmy.ramsamynaick@epitech.eu>
2025-06-23 14:18:49 +0400

    [UPDATE] read.sh

--- lundi/read.sh
+++ lundi/read.sh
@@ -14,3 +14,9 @@ read age
 echo "Bonjour, vous vous appelez $nom et vous avez $age ans."
 
 read -s -p "Entrer votre mot de passe : " motdepasse
+
+echo "Quelle est votre couleur préférée ?"
+read -t 10 couleur
+
+echo "code de retour de read : $?"
+echo "Votre couleur préférée est $couleur."
