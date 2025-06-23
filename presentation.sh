9cd0287b95adf29d5fa20b299d909ade8485765a
jimmy <jimmy.ramsamynaick@epitech.eu>
2025-06-23 10:11:28 +0400

    [ADD] presentation.sh

---
+++ presentation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# script de presentation Exercice 1
+# Utilisation ./presentation.sh
+# Script : presentation.sh
+#Auteur : Jimmy RAMSAMYNAÏCK
+# Date : $(date)
+
+echo "Bonjour, je m'appelle Jimmy RAMSAMYNAÏCK." #affiche le nom
+echo $(date) #affiche la date
+echo $(hostname) #affiche le nom de la machine
+echo $PWD #affiche le répertoire courant
