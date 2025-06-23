90cb7f3363986ddf411683f2163811f8d9353c51
jimmy <jimmy.ramsamynaick@epitech.eu>
2025-06-23 14:05:43 +0400

    [ADD] read.sh

---
+++ lundi/read.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# script
+# Utilisation ./read.sh
+# Script : read.sh
+#Auteur : Jimmy RAMSAMYNAÏCK
+# Date : $(date)
+
+echo "entrer votre nom :"
+read nom
+
+echo "entrer votre age :"
+read age
+
+echo "Bonjour, vous vous appelez $nom et vous avez $age ans."
