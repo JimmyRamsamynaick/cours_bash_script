5f8a3556a027a9b7c37c6068fc5d1afe65a665ae
jimmy <jimmy.ramsamynaick@epitech.eu>
2025-06-23 11:00:42 +0400

    [ADD] infos_systeme.sh but last don't lunch in my pc

---
+++ lundi/info_systeme.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Exercice 2
+# Script : info_systeme.sh
+#Auteur : Jimmy RAMSAMYNAÏCK
+# Date : $(date)
+
+users  # affiche les utilisateurs connectés
+df -h  # affiche l'espace disque utilisé
+uptime -p  # affiche le temps de fonctionnement du système
+last -5  # affiche les derniers logs du système
