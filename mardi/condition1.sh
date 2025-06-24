07ff7a1346fe2186d3dfcdd8cec755e06f4c167c
jimmy <jimmy.ramsamynaick@epitech.eu>
2025-06-24 09:23:36 +0400

    [UPDATE] condition1.sh

--- mardi/condition1.sh
+++ mardi/condition1.sh
@@ -9,8 +9,14 @@
 
 read -p "Entrez votre age : " age
 
-if [ $age -gt 1 ]; then
-  echo "Vous avez $age ans"
-  else [ $age -eq 1 ];
-    echo "Vous avez 1 an"
+#empecher l'utilisateur de saisir un age invalide, s'il rentre un age invalide, le script affiche une erreur et quitte.
+#un age est un chiffre entier compris entre 1 et 150
+
+if [ "$age" -lt 1 ] || [ "$age" -gt 150 ]; then
+    echo "Erreur : l'age doit etre un entier compris entre 1 et 150."
+    exit 1
+    elif [ "$age" -eq 1 ]; then
+      echo "Vous avez 1 an."
+    else
+      echo "Vous avez $age ans"
 fi
